c6b337c104c9129647ccc5e0898d7369e4e0c41d
rhuanbello <rhuambello@gmail.com>
2023-05-06 15:01:52 -0300

    [chore] Add ESLint + Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'Spotify',
-  description: 'Listen to your favorite music and podcasts on Spotify!',
-}
+  description: 'Listen to your favorite music and podcasts on Spotify!'
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="bg-zinc-900 text-zinc-400">{children}</body>
     </html>
-  )
+  );
 }
